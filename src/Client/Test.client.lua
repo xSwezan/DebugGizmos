619a1d0fdf6e92d6afdaa31b5d671203bdf77982
xSwezan <101433236+xSwezan@users.noreply.github.com>
2023-05-14 22:54:14 +0200

    Version fix

--- src/Client/Test.client.lua
+++ src/Client/Test.client.lua
@@ -15,19 +15,7 @@ task.wait(3)
 local AllTime = 0
 local DidLoop = false
 
-local StartCos
-local StartCosTimestamp
-
 RunService.RenderStepped:Connect(function(DT: number)
-	if (StartCos) then
-		if (StartCos <= (math.cos(tick()) + .01)) and (StartCos >= (math.cos(tick()) - .01)) and not (DidLoop) and (StartCosTimestamp) and (tick() - StartCosTimestamp > 1) then
-			DidLoop = true
-		end
-	else
-		StartCos = math.cos(tick())
-		StartCosTimestamp = tick()
-	end
-
 	AllTime += DT
 
 	RayLine:Apply{
@@ -42,30 +30,24 @@ RunService.RenderStepped:Connect(function(DT: number)
 		To = Vector3.new(0,math.cos(tick()) * 3,math.sin(tick()) * 4) + Vector3.new(0,math.cos(-tick()) * 5,math.sin(-tick()) * 5) + Vector3.new(0,math.cos(tick()) * 8,math.sin(tick()) * 7);
 	}
 
-	if (LastPoint1) and (math.floor(AllTime * 4) % 2 == 0) and not (DidLoop) then
-		DebugGizmos.Line{
-			From = LastPoint1;
-			To = Vector3.new(0,math.cos(tick()) * 3,math.sin(tick()) * 4) + Vector3.new(0,math.cos(-tick()) * 5,math.sin(-tick()) * 5);
-			
-			Color = Color3.fromRGB(0,255,0);
-		}
-	end
-	if (LastPoint2) and (math.floor(AllTime * 4) % 2 == 0) and not (DidLoop) then
-		DebugGizmos.Line{
-			From = LastPoint2;
-			To = Vector3.new(0,math.cos(tick()) * 3,math.sin(tick()) * 4);
-			
-			Color = Color3.fromRGB(0,0,255);
-		}
-	end
-	if (LastPoint3) and (math.floor(AllTime * 4) % 2 == 0) and not (DidLoop) then
-		DebugGizmos.Line{
-			From = LastPoint3;
-			To = Vector3.new(0,math.cos(tick()) * 3,math.sin(tick()) * 4) + Vector3.new(0,math.cos(-tick()) * 5,math.sin(-tick()) * 5) + Vector3.new(0,math.cos(tick()) * 8,math.sin(tick()) * 7);
-			
-			Color = Color3.fromRGB(255,0,0);
-		}
-	end
+	DebugGizmos.Line{
+		From = LastPoint1;
+		To = Vector3.new(0,math.cos(tick()) * 3,math.sin(tick()) * 4) + Vector3.new(0,math.cos(-tick()) * 5,math.sin(-tick()) * 5);
+		
+		Color = Color3.fromRGB(0,255,0);
+	}
+	DebugGizmos.Line{
+		From = LastPoint2;
+		To = Vector3.new(0,math.cos(tick()) * 3,math.sin(tick()) * 4);
+		
+		Color = Color3.fromRGB(0,0,255);
+	}
+	DebugGizmos.Line{
+		From = LastPoint3;
+		To = Vector3.new(0,math.cos(tick()) * 3,math.sin(tick()) * 4) + Vector3.new(0,math.cos(-tick()) * 5,math.sin(-tick()) * 5) + Vector3.new(0,math.cos(tick()) * 8,math.sin(tick()) * 7);
+		
+		Color = Color3.fromRGB(255,0,0);
+	}
 
 	LastPoint1 = Vector3.new(0,math.cos(tick()) * 3,math.sin(tick()) * 4) + Vector3.new(0,math.cos(-tick()) * 5,math.sin(-tick()) * 5)
 	LastPoint2 = Vector3.new(0,math.cos(tick()) * 3,math.sin(tick()) * 4)
